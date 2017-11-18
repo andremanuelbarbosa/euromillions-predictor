38f47da03110a756bd13617353a45dd719bdcf63
Andre Barbosa <andremanuelbarbosa@gmail.com>
2017-11-18 18:08:47 +0000

    Added Draw 1060.

--- src/main/resources/dumps/dump.sh
+++ src/main/resources/dumps/dump.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
 export PGPASSWORD='postgres';
-pg_dump -Fc -U postgres -t draws_stats_stars -t draws_stats_numbers -t draws_stats_intervals_stars -t draws_stats_intervals_numbers -f src/main/resources/dumps/DrawStatsUpToDraw1059.dmp euromillions-predictor
-pg_dump -Fc -U postgres -t draws_templates_stars -t draws_templates_numbers -f src/main/resources/dumps/DrawTemplatesUpToDraw1059.dmp euromillions-predictor
-pg_dump -Fc -U postgres -t formulas_stats -f src/main/resources/dumps/FormulaStatsUpToDraw1059.dmp euromillions-predictor
+pg_dump -Fc -U postgres -t draws_stats_stars -t draws_stats_numbers -t draws_stats_intervals_stars -t draws_stats_intervals_numbers -f src/main/resources/dumps/DrawStatsUpToDraw1060.dmp euromillions-predictor
+pg_dump -Fc -U postgres -t draws_templates_stars -t draws_templates_numbers -f src/main/resources/dumps/DrawTemplatesUpToDraw1060.dmp euromillions-predictor
+pg_dump -Fc -U postgres -t formulas_stats -f src/main/resources/dumps/FormulaStatsUpToDraw1060.dmp euromillions-predictor
